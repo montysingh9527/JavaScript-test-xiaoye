16899416ccaafff0641fa48c0d5caca821dfc826
marvin <xiaomarvin9527@gmail.com>
2023-02-28 01:29:12 +0800

    样式编写

--- javascript-class/app.js
+++ javascript-class/app.js
@@ -1,1 +1,7 @@
-console.log("2222")
+import Fade from "./carousel/fade";
+
+new Fade("#carousel-fade",{
+    defaultIndex: 0, // 默认选中
+    duration: 3000, //  轮播时间
+    
+});
---
+++ javascript-class/carousel/fade.js
@@ -0,0 +1,11 @@
+import "./css/resets.css";
+import "./css/fade.scss";
+
+export default class Fade{
+    constructor(el, {
+        defaultIndex,    // 当前选中项
+        duration,  //  轮播时间
+    })
+}
+
+
